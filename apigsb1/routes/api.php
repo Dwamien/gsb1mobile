d0a47496d32489a8c3e139c80bd3fea3aacbd5ae
Dwamien <damien.farault@gmail.com>
2020-10-19 14:19:08 +0200

    api rest : authentification ok

--- apigsb1/routes/api.php
+++ apigsb1/routes/api.php
@@ -14,8 +14,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::apiresource('Composition', 'CompositionController');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['middleware' => 'auth:api'], function(){
+    Route::apiresource('Composition', 'CompositionController');
+    Route::get('logout', 'Auth\LoginController@logout');
+});
+
+Route::post('login', 'Auth\LoginController@login');
+Route::post('register', 'Auth\RegisterController@register');
