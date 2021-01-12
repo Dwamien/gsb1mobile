06c4ee26aa690f621749d7629ac77e5fe4a39946
Dwamien <damien.farault@gmail.com>
2021-01-12 01:11:29 +0100

    cordova

--- gsb1/src/app/services/shared.service.ts
+++ gsb1/src/app/services/shared.service.ts
@@ -8,8 +8,8 @@ import { User } from '../models/user';
 export class SharedService {
   public isConnected: boolean;
   public user: User;
-  public URLAPI: string = "http://127.0.0.1:8000/api/";
-
+  //public URLAPI: string = "http://127.0.0.1:8000/api/";
+  public URLAPI: string= "http://10.10.16.8/user1/apigsb1/public/api/";
 
   constructor() { }
 
